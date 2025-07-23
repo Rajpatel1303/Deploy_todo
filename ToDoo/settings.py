5a291250f1e94c47e6989492259876a7e3a41c10
Sakuni1303 <project130399@gmail.com>
2025-07-23 11:26:13 +0530

    Add STATIC_ROOT for production deployment

--- ToDoo/settings.py
+++ ToDoo/settings.py
@@ -136,6 +136,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),  # ✅ Your static folder
 ]
